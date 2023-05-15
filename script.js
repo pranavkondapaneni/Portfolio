8d13eca187184d9e9207a514bf295b7c44153a3f
Pranav Kondapaneni <pranavkondapaneni@gmail.com>
2023-05-14 19:13:17 -0500

    emptying

--- script.js
+++
@@ -1,9 +0,0 @@
-function revealMessage(){
-    document.getElementById("hiddenMessage").style.display = 'block';
-}
-
-function decrement(){
-    var num = document.getElementById("number").innerHTML;
-    num -= 1;
-    document.getElementById("number").innerHTML = num
-}
