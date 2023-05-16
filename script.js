bc2882f9dacae5be6f7ee3773fecf6758d95da8a
Pranav Kondapaneni <pranavkondapaneni@gmail.com>
2023-05-16 13:36:13 -0500

    Navbar

---
+++ script.js
@@ -0,0 +1,23 @@
+const [red, green, blue] = [0, 24, 183]
+const main = document.querySelector('.main')
+
+function changeColorOnScroll(){
+    const scrolled = 1 + (window.scrollY)/200
+    const [r, g, b] = [red/scrolled, green/scrolled, blue/scrolled].map(Math.round)
+    main.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+}
+
+window.addEventListener('scroll', changeColorOnScroll)
+
+var i = 0
+var mainName = 'Pranav Kondapaneni'
+
+// Change to typing effect with cursor.
+function typingEffect(){
+    if(i < mainName.length){
+        document.getElementById('name').innerHTML += mainName.charAt(i)
+        i++
+        setTimeout(typingEffect, 100)
+    }
+}
+setTimeout(typingEffect, 100)
