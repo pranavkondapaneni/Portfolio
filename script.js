cd7812c2e0f0fd82ccd3547d8d3cf3c77aa833f5
Pranav Kondapaneni <pranavkondapaneni@gmail.com>
2023-05-17 17:27:17 -0500

    Commit Before Linux Switch

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const [red, green, blue] = [0, 24, 183]
 const main = document.querySelector('.body')
 
 function changeColorOnScroll(){
-    const scrolled = 1 + (window.scrollY)/200
+    const scrolled = 1 + (window.scrollY)/95
     const [r, g, b] = [red/scrolled, green/scrolled, blue/scrolled].map(Math.round)
     main.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 }
-
 window.addEventListener('scroll', changeColorOnScroll)
 
 var i = 0
